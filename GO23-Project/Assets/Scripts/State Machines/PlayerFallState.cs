3afc6160c3ab8f8b305c07d279cc907345a6bfe5
kwlucas <lucaswkyle@gmail.com>
2023-11-11 09:58:08 -0700

    Added comments on fall and jump states

--- GO23-Project/Assets/Scripts/State Machines/PlayerFallState.cs
+++ GO23-Project/Assets/Scripts/State Machines/PlayerFallState.cs
@@ -12,6 +12,7 @@ public class PlayerFallState : PlayerState
     public override void Enter()
     {
         base.Enter();
+        //Set a horizontal speed capacity based on either the speed when entering the sate or 5, whatever is greater
         initialSpeed = Mathf.Abs(playerController.Body.velocity.x);
         speedCap = (initialSpeed >= 5.0f) ? initialSpeed : 5.0f;
     }
@@ -19,19 +20,22 @@ public class PlayerFallState : PlayerState
     public override void FrameUpdate()
     {
         base.FrameUpdate();
+        //Air control
         float movementInput = Input.GetAxis("Horizontal");
+        playerController.Body.AddForce(Vector2.right * movementInput * (playerController.moveForce / 2));
+        if (movementInput > 0) playerController.Sprite.flipX = false;
+        else if (movementInput < 0) playerController.Sprite.flipX = true;
+        //Exit fall state when ground is detected
         if (playerController.GroundCheck() && playerController.Body.velocity.y < 0.01f)
         {
             stateMachine.ChangeState(playerController.IdleState);
         }
-        playerController.Body.AddForce(Vector2.right * movementInput * (playerController.moveForce / 2));
-        if (movementInput > 0) playerController.Sprite.flipX = false;
-        else if (movementInput < 0) playerController.Sprite.flipX = true;
     }
 
     public override void PhysicsUpdate()
     {
         base.PhysicsUpdate();
+        //Ensure horizontal velocity is within speed capacity
         playerController.SetVelocityX(Mathf.Clamp(playerController.Body.velocity.x, -speedCap, speedCap));
     }
 }
--- GO23-Project/Assets/Scripts/State Machines/PlayerJumpState.cs
+++ GO23-Project/Assets/Scripts/State Machines/PlayerJumpState.cs
@@ -16,14 +16,17 @@ public class PlayerJumpState : PlayerAbilityState
     public override void FrameUpdate()
     {
         base.FrameUpdate();
+        //Air control
         float movementInput = Input.GetAxis("Horizontal");
         playerController.Body.AddForce(Vector2.right * movementInput * (playerController.moveForce / 2));
         if (movementInput > 0) playerController.Sprite.flipX = false;
         else if (movementInput < 0) playerController.Sprite.flipX = true;
+        //Variable jump height
         if (runtime < playerController.jumpTime && Input.GetButton("Jump"))
         {
             playerController.Body.AddForce(Vector2.up * playerController.jumpForce * 1.66f);
         }
+        //Exiting jump state when no longer moving up
         if (playerController.Body.velocity.y <= 0.0f)
         {
             abilityTriggered = true;
