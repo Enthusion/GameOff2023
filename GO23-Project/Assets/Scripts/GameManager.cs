266d78b319c7eb0e961775dd621c124de783cefb
kwlucas <lucaswkyle@gmail.com>
2023-11-16 13:25:25 -0700

    Formatting

--- GO23-Project/Assets/Scripts/GameManager.cs
+++ GO23-Project/Assets/Scripts/GameManager.cs
@@ -4,32 +4,38 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
+    public static GameManager Instance { get; private set; }
     private float vitaEnergy;
     private float mortEnergy;
 
-    public void Awake(){
-        if(Instance == null){
+    public void Awake()
+    {
+        if (Instance == null)
+        {
             Instance = this;
         }
     }
 
-    public float UpdateEnergy(float newValue, int playerId){
+    public float UpdateEnergy(float newValue, int playerId)
+    {
         float characterBalance = 0;
-        if(playerId == 0){
+        if (playerId == 0)
+        {
             vitaEnergy = newValue;
             characterBalance = 1;
         }
-        else if( playerId == 1){
+        else if (playerId == 1)
+        {
             mortEnergy = newValue;
             characterBalance = -1;
         }
         return characterBalance * GetBalance();
     }
 
-    public float GetBalance(){
+    public float GetBalance()
+    {
         float greaterEnergy = vitaEnergy > mortEnergy ? vitaEnergy : mortEnergy;
         if (greaterEnergy == 0) return 0;
-        return (vitaEnergy - mortEnergy)/greaterEnergy;
+        return (vitaEnergy - mortEnergy) / greaterEnergy;
     }
 }
