90cca0a8c4e8d84a320bb513822d084935390079
kwlucas <lucaswkyle@gmail.com>
2023-11-16 15:32:45 -0700

    Implemented LivingEntity functionality

--- GO23-Project/Assets/Scripts/LivingEntity.cs
+++ GO23-Project/Assets/Scripts/LivingEntity.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class LivingEntity : KillableEntity
 {
-    
+    protected Rigidbody2D Body;
+    protected Vector2 damageDriection;
+    public override void Start()
+    {
+        base.Start();
+        Body = GetComponent<Rigidbody2D>();
+
+        damageDriection = Vector2.zero;
+    }
+    public override void TakeDamage(float damageValue, GameObject damageSource)
+    {
+        base.TakeDamage(damageValue, damageSource);
+        damageDriection = damageSource.transform.position - transform.position;
+        damageDriection = damageDriection.normalized;
+
+    }
 }
