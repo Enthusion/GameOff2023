2848082686dd7b235d1ea492db4a3ed7a2a21fb5
kwlucas <lucaswkyle@gmail.com>
2023-11-16 12:19:04 -0700

    Initialized game manager

--- GO23-Project/Assets/Scripts/GameManager.cs
+++ GO23-Project/Assets/Scripts/GameManager.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+
+    public void Awake(){
+        if(Instance == null){
+            Instance = this;
+        }
+    }
 }
