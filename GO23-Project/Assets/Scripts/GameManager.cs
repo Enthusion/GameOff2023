7289ecbe2f29c71303384eb94b594286bed082fe
kwlucas <lucaswkyle@gmail.com>
2023-11-16 13:10:37 -0700

    Added energy and balance stats to gameManager

--- GO23-Project/Assets/Scripts/GameManager.cs
+++ GO23-Project/Assets/Scripts/GameManager.cs
@@ -5,10 +5,31 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    private float vitaEnergy;
+    private float mortEnergy;
 
     public void Awake(){
         if(Instance == null){
             Instance = this;
         }
+    }
+
+    public float UpdateEnergy(float newValue, int playerId){
+        float characterBalance = 0;
+        if(playerId == 0){
+            vitaEnergy = newValue;
+            characterBalance = 1;
+        }
+        else if( playerId == 1){
+            mortEnergy = newValue;
+            characterBalance = -1;
+        }
+        return characterBalance * GetBalance();
+    }
+
+    public float GetBalance(){
+        float greaterEnergy = vitaEnergy > mortEnergy ? vitaEnergy : mortEnergy;
+        if (greaterEnergy == 0) return 0;
+        return (vitaEnergy - mortEnergy)/greaterEnergy;
     }
 }
