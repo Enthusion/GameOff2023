8bd94d1586ef5a8beeb3b5d1fecf77c442ccc084
kwlucas <lucaswkyle@gmail.com>
2023-11-16 14:53:33 -0700

    Implemented base functionality

--- GO23-Project/Assets/Scripts/KillableEntity.cs
+++ GO23-Project/Assets/Scripts/KillableEntity.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class KillableEntity : MonoBehaviour, IDamageable
 {
-    public float health{ get; protected set;}
-    public float baseHealth{ get; protected set;}
+    protected float health;
+    protected Collider2D Collider;
 
-    public virtual void TakeDamage(float damageValue, GameObject damageSource){}
+    public virtual void Start(){
+        if(TryGetComponent<BoxCollider2D>(out BoxCollider2D colliderA)){
+            Collider = colliderA;
+        }
+        else if(TryGetComponent<CapsuleCollider2D>(out CapsuleCollider2D colliderB)){
+            Collider = colliderB;
+        }
+        else if(TryGetComponent<CircleCollider2D>(out CircleCollider2D colliderC)){
+            Collider = colliderC;
+        }
+    }
+    public virtual void TakeDamage(float damageValue, GameObject damageSource){
+        health -= damageValue;
+        if(health <= 0){
+            Die();
+        }
+    }
 
     public virtual void Die(){}
 }
