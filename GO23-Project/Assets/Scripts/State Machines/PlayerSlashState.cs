99bf1ddf6c57a3725330a196f34bf23f84f89fe2
kwlucas <lucaswkyle@gmail.com>
2023-11-17 11:39:36 -0700

    Created PlayerSlashState

---
+++ GO23-Project/Assets/Scripts/State Machines/PlayerSlashState.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSlashState : PlayerAbilityState
+{
+    public PlayerSlashState(Controller controller, StateMachine stateMachine, string stateName) : base(controller, stateMachine, stateName)
+    { }
+}
