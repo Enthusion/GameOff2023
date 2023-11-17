dfbd102b8e3977cae57712490982c504e2e5e872
kwlucas <lucaswkyle@gmail.com>
2023-11-17 10:25:04 -0700

    Created EnemyData Scriptable Object

---
+++ GO23-Project/Assets/Scripts/EnemyData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New Enemy Data", menuName="Enemy Data")]
+public class EnemyData : ScriptableObject
+{
+    
+}
