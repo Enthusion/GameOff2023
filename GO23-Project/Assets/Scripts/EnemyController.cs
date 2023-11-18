41c61ae23d32e2bc455a72b89946fcbd6f2cd09c
kwlucas <lucaswkyle@gmail.com>
2023-11-17 20:45:31 -0700

    Prepped for enemy implementation

--- GO23-Project/Assets/Scripts/EnemyController.cs
+++ GO23-Project/Assets/Scripts/EnemyController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    private Rigidbody2D Body;
+    public EnemyData enemyData;
+    private int enemyId;
+    private int aggressionLevel; //0 is non attack, 1 is attack back, 2 is on sight
+    private float energyValue; //Energy given to palyers on hit
+    private bool contactDamage; //Does the enemy do contact damage
+    private float damageAmount; //How much damage does the enemy do
+    private bool stationary; //Does the stay in one place?
+    private float moveForce; //The force the enemy moves with
+    private float patrolRange; //How far back and forth between the original spawn point the enemy will patrol
+    private float lineOfSight; //How far an enemy can see
+    private float detectionProximity; //How close umtil the enemy automatically knows the player is there.
+    private float edgeDetection; //Can the enemy see edges
+
+    //Awake is called as soon as the script is loaded, before start
+    void Awake(){
+        enemyId = enemyData.enemyId;
+        aggressionLevel = enemyData.enemyId;
+        energyValue = enemyData.energyValue;
+        contactDamage = enemyData.contactDamage;
+        damageAmount = enemyData.damageAmount;
+        stationary = enemyData.stationary;
+        moveForce = enemyData.moveForce;
+        patrolRange = enemyData.patrolRange;
+        lineOfSight = enemyData.lineOfSight;
+        detectionProximity = enemyData.detectionProximity;
+        edgeDetection = enemyData.edgeDetection;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //TODO: If enemy is not within BLANK distance to target run MoveTowardTarget function
         
     }
+
+    //TODO: Set up MoveTowardTarget function that calculates the direction of the target and moves toward them.
 }
--- GO23-Project/Assets/Scripts/EnemyData.cs
+++ GO23-Project/Assets/Scripts/EnemyData.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName="New Enemy Data", menuName="Enemy Data")]
 public class EnemyData : ScriptableObject
 {
+    public string enemyName;
     public int enemyId;
     public int aggressionLevel; //0 is non attack, 1 is attack back, 2 is on sight
     public float energyValue; //Energy given to palyers on hit
     public bool contactDamage; //Does the enemy do contact damage
+    public float damageAmount; //How much damage does the enemy do
     public bool stationary; //Does the stay in one place?
     public float moveForce = 8.0f;
     public float patrolRange; //How far back and forth between the original spawn point the enemy will patrol
-    public float LineOfSight; //How far an enemy can see
+    public float lineOfSight; //How far an enemy can see
     public float detectionProximity; //How close umtil the enemy automatically knows the player is there.
     public float edgeDetection; //Can the enemy detect edges
+    // public GameObject attackObject; //The attack prefab the enemy uses
+    // public float attackOffestX; //The X offset of the attack spawn point
+    // public float attackOffsetY; //The Y offset of the attack spawn point
+    // public float attackCooldown; //Time between each attack
+    // public int multiAttack = 0; //How many attacks does one attack contain
+    // public float burstCooldown; //What is the time between each attack in a multi-attack
 }
