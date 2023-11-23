9f978661c4594223eb608d99a079ff179969b81d
kwlucas <lucaswkyle@gmail.com>
2023-11-23 13:02:40 -0700

    Implemented force exit follow state if unallowed

--- GO23-Project/Assets/Scripts/Barrier.cs
+++ GO23-Project/Assets/Scripts/Barrier.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class Barrier : MonoBehaviour
 {
     public GameObject AllowedCharacter;
+    private PlayerController allowedPlayer;
     private BoxCollider2D barrierCollider;
+
 
     public void Start(){
         barrierCollider = GetComponent<BoxCollider2D>();
+        allowedPlayer = AllowedCharacter.GetComponent<PlayerController>();
         CapsuleCollider2D playerCollider = AllowedCharacter.GetComponent<CapsuleCollider2D>();
         Physics2D.IgnoreCollision(barrierCollider, playerCollider, true);
+    }
+    public void OnCollisionEnter2D(Collision2D col){
+        if (col.gameObject.TryGetComponent(out PlayerController player)){
+            if(player.characterId != allowedPlayer.characterId){
+                if(player.Following){
+                    player.ForceToWaiting();
+                }
+            }
+        }
     }
 }
