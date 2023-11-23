8f37f5cda68112976eb70d9c4eea7d184117d15d
kwlucas <lucaswkyle@gmail.com>
2023-11-23 12:51:36 -0700

    Created working charatcer specific barrier

--- GO23-Project/Assets/Scripts/Barrier.cs
+++ GO23-Project/Assets/Scripts/Barrier.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class Barrier : MonoBehaviour
 {
-    public int AllowCharacter;
+    public GameObject AllowedCharacter;
+    private BoxCollider2D barrierCollider;
+
+    public void Start(){
+        barrierCollider = GetComponent<BoxCollider2D>();
+        CapsuleCollider2D playerCollider = AllowedCharacter.GetComponent<CapsuleCollider2D>();
+        Physics2D.IgnoreCollision(barrierCollider, playerCollider, true);
+    }
 }
