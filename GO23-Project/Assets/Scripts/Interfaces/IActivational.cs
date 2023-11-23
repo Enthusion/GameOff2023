c23952f2b428cd9d6f6eb0f671ef3eef1e425eda
kwlucas <lucaswkyle@gmail.com>
2023-11-23 13:36:21 -0700

    Set up activational interface

--- GO23-Project/Assets/Scripts/Interfaces/IActivational.cs
+++ GO23-Project/Assets/Scripts/Interfaces/IActivational.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IActivational : MonoBehaviour
+public interface IActivational
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Activating();
+    void Activated();
 }
