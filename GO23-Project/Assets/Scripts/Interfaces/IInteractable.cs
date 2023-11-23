658946a09e99128aa17142e99df943cf280debaa
kwlucas <lucaswkyle@gmail.com>
2023-11-23 13:32:07 -0700

    Set up interact interface

--- GO23-Project/Assets/Scripts/Interfaces/IInteractable.cs
+++ GO23-Project/Assets/Scripts/Interfaces/IInteractable.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class IInteractable : MonoBehaviour
+public interface IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Interact(PlayerController interactingPlayer);
 }
