c077fad4a461e62ecc4156a8663b0677510a48ef
kwlucas <lucaswkyle@gmail.com>
2023-11-23 17:22:34 -0700

    Set up activation functions

--- GO23-Project/Assets/Scripts/SwitchReciever.cs
+++ GO23-Project/Assets/Scripts/SwitchReciever.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,31 @@ public class SwitchReciever : MonoBehaviour
     protected int neededActivations;
     protected int currentActivations;
     protected bool activatedAtStart;
+    protected bool initialized;
 
-    public virtual void Awake(){
+    public virtual void Awake()
+    {
         neededActivations = 0;
         currentActivations = 0;
+        initialized = false;
     }
-    public virtual void StartUp(bool isActive){
+    public virtual void StartUp(bool isActive)
+    {
         neededActivations++;
-        if(isActive) currentActivations++;
+        if (isActive) currentActivations++;
         activatedAtStart = neededActivations == currentActivations;
     }
+    public virtual void Start() { }
+    public virtual void Update()
+    {
+        if (!initialized && activatedAtStart) Activated();
+    }
+    public virtual void FixedUpdate() { }
+    public virtual void ReciveActivator(int switchWeight = 1)
+    {
+        currentActivations += switchWeight;
+        if (currentActivations >= neededActivations) Activating();
+    }
+    public virtual void Activating() { }
+    public virtual void Activated() { }
 }
