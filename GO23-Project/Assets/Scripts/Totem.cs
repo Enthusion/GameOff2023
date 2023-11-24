130058aaf5223f90f3a0f4b5b4c0e9a7df647816
kwlucas <lucaswkyle@gmail.com>
2023-11-23 18:33:40 -0700

    Set up interact function

--- GO23-Project/Assets/Scripts/Totem.cs
+++ GO23-Project/Assets/Scripts/Totem.cs
@@ -13,4 +13,23 @@ public class Totem : Switch
         if(GameManager.Instance.CheckSwitches(SwitchID)) Activated();
         base.Start();
     }
+
+    public virtual void Interact(PlayerController interactingPlayer){
+        if(CharacterSpecificID != -1 && interactingPlayer.characterId != CharacterSpecificID){
+            //Display message about character needed to activate?
+            return;
+        }
+
+        if(EnergyRequisite != 0){
+            float neededEnergy = currentEnergy - EnergyRequisite;
+            float energyChange = interactingPlayer.currentEnergy - neededEnergy >= 0?neededEnergy:interactingPlayer.currentEnergy;
+            currentEnergy +=  energyChange;
+            interactingPlayer.AdjustEnergy(-energyChange);
+            if(currentEnergy == EnergyRequisite){
+                Activating();
+                return;
+            }
+            //Display message about energy needed?
+        }
+    }
 }
