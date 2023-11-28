d91559abacd7d0b9ad831f4dd78ff8c860c6ceab
kwlucas <lucaswkyle@gmail.com>
2023-11-27 17:13:50 -0700

    Implemented Totem/Activator functionality

--- GO23-Project/Assets/Scripts/Totem.cs
+++ GO23-Project/Assets/Scripts/Totem.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Totem : Switch
+public class Totem : Switch, IInteractable
 {
     public string SwitchID;
+    public int switchWeight = 1;
     public float EnergyRequisite = 0;
     public int CharacterSpecificID = -1;
     private float currentEnergy;
@@ -16,24 +17,40 @@ public class Totem : Switch
 
     public virtual void Interact(PlayerController interactingPlayer)
     {
-        if (CharacterSpecificID != -1 && interactingPlayer.characterId != CharacterSpecificID)
+        if (!Active)
         {
-            //Display message about character needed to activate?
-            return;
-        }
-
-        if (EnergyRequisite != 0)
-        {
-            float neededEnergy = currentEnergy - EnergyRequisite;
-            float energyChange = interactingPlayer.currentEnergy - neededEnergy >= 0 ? neededEnergy : interactingPlayer.currentEnergy;
-            currentEnergy += energyChange;
-            interactingPlayer.AdjustEnergy(-energyChange);
-            if (currentEnergy == EnergyRequisite)
+            if (CharacterSpecificID != -1 && interactingPlayer.characterId != CharacterSpecificID)
             {
-                Activating();
+                //Display message about character needed to activate?
                 return;
             }
-            //Display message about energy needed?
+
+            if (EnergyRequisite != 0)
+            {
+                float neededEnergy = currentEnergy - EnergyRequisite;
+                float energyChange = interactingPlayer.currentEnergy - neededEnergy >= 0 ? neededEnergy : interactingPlayer.currentEnergy;
+                currentEnergy += energyChange;
+                interactingPlayer.AdjustEnergy(-energyChange);
+                if (currentEnergy == EnergyRequisite)
+                {
+                    Activating();
+                    return;
+                }
+                //Display message about energy needed?
+                return;
+            }
+            Activating();
         }
+    }
+
+    public override void Activating()
+    {
+        base.Activating();
+        Reciever.ReciveActivator(switchWeight);
+    }
+
+    public override void Activated()
+    {
+        base.Activated();
     }
 }
