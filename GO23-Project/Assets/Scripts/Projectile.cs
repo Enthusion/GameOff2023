2c95b6c6a01d055aba897803d3b1c3a05fd3f6c1
kwlucas <lucaswkyle@gmail.com>
2023-11-28 13:26:27 -0700

    Made hit functionality

--- GO23-Project/Assets/Scripts/Projectile.cs
+++ GO23-Project/Assets/Scripts/Projectile.cs
@@ -7,16 +7,48 @@ public class Projectile : MonoBehaviour
 {
     public float Speed;
     public float Range;
+    public float KnockbackForce = 0;
     protected float damage;
     protected GameObject origin;
     // protected Collider2D projectileCollider;
     protected Rigidbody2D body;
     protected float lifeTime = 3;
+    protected bool useConstantVelocity = true;
 
 
     public virtual void Start()
     {
         body = GetComponent<Rigidbody2D>();
+    }
+
+    public virtual void Update()
+    {
+        if (useConstantVelocity)
+        {
+            body.velocity = transform.right * Speed;
+        }
+    }
+
+    public virtual void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.TryGetComponent(out IDamageable damageableEntity))
+        {
+            if (KnockbackForce > 0)
+            {
+                Rigidbody2D entityBody = col.attachedRigidbody;
+                if (entityBody != null)
+                {
+                    Vector2 collisionDir = entityBody.position - body.position;
+                    if (col.gameObject.TryGetComponent(out PlayerEntity player))
+                    {
+                        player.KnockbackInfo(collisionDir * (KnockbackForce / 2));
+                    }
+                    else entityBody.AddForce(collisionDir.normalized * KnockbackForce, ForceMode2D.Impulse);
+
+                }
+            }
+            damageableEntity.TakeDamage(damage, origin);
+        }
     }
 
     public virtual void Initialization(GameObject sourceObj, float damageVal, bool overideStats = false, float newSpeed = 1, float newRange = 10)
