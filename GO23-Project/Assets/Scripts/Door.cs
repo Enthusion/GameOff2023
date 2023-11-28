215599ffb6764a44c6d75975a6194e649a8beb02
kwlucas <lucaswkyle@gmail.com>
2023-11-27 17:01:20 -0700

    Set up door functionality

--- GO23-Project/Assets/Scripts/Door.cs
+++ GO23-Project/Assets/Scripts/Door.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Door : SwitchReciever
 {
-    
+    protected Animator anima;
+
+    public override void Start()
+    {
+        base.Start();
+        anima = GetComponent<Animator>();
+    }
+    public override void Activating()
+    {
+        base.Activating();
+        Activated();
+    }
+    public override void Activated()
+    {
+        base.Activated();
+        anima.SetBool("Open", true);
+    }
 }
