a131a4c34e7b55cfc67e6d7e17ad9e2b65be11db
kwlucas <lucaswkyle@gmail.com>
2023-11-30 02:55:21 -0700

    Created space checker functionality

--- GO23-Project/Assets/Scripts/SpaceChecker.cs
+++ GO23-Project/Assets/Scripts/SpaceChecker.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpaceChecker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float MinimumScale;
+    public float CrushDistance;
+    private Vector2 center;
+    public void Start(){
+        center = transform.position;
+    }
+    public void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.TryGetComponent<PlayerController>(out PlayerController character)){
+            if (character.transform.localScale.x > MinimumScale){
+                if(character.Following){
+                    character.ForceToWaiting();
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnTriggerStay2D(Collider2D col){
+        if(col.gameObject.TryGetComponent<PlayerEntity>(out PlayerEntity character)){
+            if (character.transform.localScale.x > MinimumScale && Vector2.Distance(character.transform.position, transform.position) <= CrushDistance){
+                character.TakeDamage(5, gameObject);
+                character.Respawn();
+            }
+        }
     }
 }
