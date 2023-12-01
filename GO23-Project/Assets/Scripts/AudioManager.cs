52f005fe7080f414e43f035ca3ac6bc1c5429da4
kwlucas <lucaswkyle@gmail.com>
2023-12-01 04:27:33 -0700

    Created the audio manager

---
+++ GO23-Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource Audio1;
+    public AudioSource Audio2;
+}
