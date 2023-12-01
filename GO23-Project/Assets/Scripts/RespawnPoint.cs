d57395b5ca83ef8d8fcb0549309bcbd248204e4a
kwlucas <lucaswkyle@gmail.com>
2023-12-01 02:44:21 -0700

    Set up respawn point script

--- GO23-Project/Assets/Scripts/RespawnPoint.cs
+++ GO23-Project/Assets/Scripts/RespawnPoint.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class RespawnPont : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnTriggerEnter2D(Collider2D col){
+        if(TryGetComponent<PlayerController>(out PlayerController player)){
+            GameManager.Instance.SetRespawnPoint(player.characterId, transform.position);
+        }
     }
 }
